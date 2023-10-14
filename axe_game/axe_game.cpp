a1205e9d69812ebf8b4b99ed3743112dccda1da4
Yenleng <yenlengggggg@gmail.com>
2023-10-14 16:31:02 +0800

    add circle and axe edges

--- axe_game/axe_game.cpp
+++ axe_game/axe_game.cpp
@@ -4,16 +4,32 @@
 int main() {
 
     // window dimensions
-    int width {350};
-    int height {200};
+    int width {800};
+    int height {450};
 
     // circle coordinates
-    int circle_x {175};
-    int circle_y {100};
+    int circle_x {200};
+    int circle_y {200};
+    int circle_radius {25};
 
-    // float root_beer {1.99};
-    // double cheese_burger {5.99};
-    // bool shouldHaveLunch {true};
+    // circle edges
+    int l_circle_x {circle_x - circle_radius};
+    int r_circle_x {circle_x + circle_radius};
+    int u_circle_y {circle_y - circle_radius};
+    int b_circle_y {circle_y + circle_radius};
+
+
+    // axe coordinates
+    int axe_x {400};
+    int axe_y {0};
+    int direction {7};
+    int axe_length {50};
+
+    // axe edges]
+    int l_axe_x {axe_x};
+    int r_axe_x {axe_x + axe_length};
+    int u_axe_y {axe_y};
+    int b_axe_y {axe_y +  axe_length};
 
     InitWindow(width, height, "Yenleng's Window");
 
@@ -24,19 +40,29 @@ int main() {
 
         // Game Logic Begins
 
-        DrawCircle(circle_x, circle_y, 25, BLUE);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+        // move the axe
+        axe_y += direction;
+        if (axe_y > height || axe_y < 0) {
+            direction = -direction;
+        } 
+
+
         // so that circle reaches end of window
-        if(IsKeyDown(KEY_D) && circle_x < 350) {
-            circle_x = circle_x + 7;
+        if(IsKeyDown(KEY_D) && circle_x < width) {
+            circle_x += 7;
         }
         if(IsKeyDown(KEY_A) && circle_x > 0) {
-            circle_x = circle_x - 7;
+            circle_x -= 7;
         }
         if(IsKeyDown(KEY_W) && circle_y > 0) {
-            circle_y = circle_y - 7;
+            circle_y -= 7;
         }
-        if(IsKeyDown(KEY_S) && circle_y < 200) {
-            circle_y = circle_y + 7;
+        if(IsKeyDown(KEY_S) && circle_y < height) {
+            circle_y += 7;
         }
 
         // Game Logic Ends
